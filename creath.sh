b8aed78eb41ff4edbade95a1589f59c168be0513
daoist1037 <zhilu826@gmail.com>
2022-06-05 14:38:34 +0800

    delte rofi

--- creath.sh
+++
@@ -1,5 +0,0 @@
-ln -s /home/daoist/dotfiles/kitty/ /home/daoist/.config/ &
-ln -s /home/daoist/dotfiles/picom/ /home/daoist/.config/ &
-ln -s /home/daoist/dotfiles/rofi/ /home/daoist/.config/ &
-ln -s /home/daoist/dotfiles/bspwm/ /home/daoist/.config/ &
-ln -s /home/daoist/dotfiles/polybar/ /home/daoist/.config/ &
